54409c2577cc8684f43024c17876a4349d6e3018
RESHAB GUPTA <b1600862nitsikkim.ac.in>
2020-09-09 13:04:48 +0530

    router

--- src/containers/Blog/FullPost/FullPost.js
+++ src/containers/Blog/FullPost/FullPost.js
@@ -12,6 +12,7 @@ class FullPost extends Component {
     componentDidUpdate(){
         if(this.props.id){
             if(!this.state.loadedPost || (this.state.loadedPost.id !== this.props.id) ){
+                
                 axios.get('/posts/' + this.props.id)
                      .then(response =>{
                             this.setState({loadedPost : response.data});
